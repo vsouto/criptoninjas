709648a55374dfdf4deb1c73e4c99d66703423d0
Victor Souto <souto.victor@gmail.com>
2018-03-01 14:11:11 -0300

    fix

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -66,7 +66,7 @@
             <!--Brand logo & name-->
             <!--================================-->
             <div class="navbar-header">
-                <a href="{{ route('dashboard') }}" class="navbar-brand">
+                <a href="{{ route('home') }}" class="navbar-brand">
                     <img src="{{ asset('img/criptoninja-white.png') }}" alt="Cripto Ninja" class="brand-icon">
                     <div class="brand-title">
                         <span class="brand-text">CriptoNinja</span>
